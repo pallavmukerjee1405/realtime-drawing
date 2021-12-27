7ab91a461a891926696fe75adf02620e55cc1de0
pallavmukerjee1405 <81314117+pallavmukerjee1405@users.noreply.github.com>
2021-12-27 12:25:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function setup(){
     video=createCapture(VIDEO);
     video.size(550,500);
 
-    canvas=createCanvas(550,550);
+    canvas=createCanvas(550,500);
     canvas.position(560,150);
 
     poseNet=ml5.poseNet(video,modelLoaded);
@@ -21,10 +21,22 @@ function modelLoaded(){
 
 function draw(){
     background('#888888');
+    document.getElementById("square_size").innerHTML="Width and Height of the Square will be = "+difference+" px";
+    fill('#E11584');
+    stroke('#E11584');
+    square(noseX,noseY,difference);
 }
 
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("NoseX - "+noseX+","+"NoseY"+noseY);
+
+        leftwristX=results[0].pose.leftWrist.x;
+        rightwristX=results[0].pose.rightWrist.x;
+        difference=floor(leftwristX-rightwristX);
+        console.log("LeftWristX - "+leftwristX+", RightWristX - "+rightwristX+"Difference - "+difference);
     }
 }
